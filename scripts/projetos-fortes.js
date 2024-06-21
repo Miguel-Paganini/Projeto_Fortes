5b8a2cea9decaf8429f9399702faa76fb8117ed5
Miguel-Paganini <128531851+Miguel-Paganini@users.noreply.github.com>
2024-06-21 16:27:38 -0300

    Add files via upload

---
+++ scripts/projetos-fortes.js
@@ -0,0 +1,9 @@
+function abrirProjeto() {
+    const box = document.getElementById('projeto_box');
+    box.style.display = 'flex';
+}
+
+function fecharProjeto() {
+    const box = document.getElementById('projeto_box');
+    box.style.display = 'none';
+}
